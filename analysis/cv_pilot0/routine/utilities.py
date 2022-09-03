b3bf4d23f032b23b477cee53b2843fd28b143c7a
phildong <zhe.dong@icahn.mssm.edu>
2022-09-03 13:56:39 -0400

    update utilities function

--- analysis/cv_pilot0/routine/utilities.py
+++ analysis/cv_pilot0/routine/utilities.py
@@ -1,6 +1,52 @@
+import itertools as itt
+import os
+import re
+
 import numpy as np
 
 
 def norm(a):
     amax, amin = np.nanmax(a), np.nanmin(a)
     return (a - amin) / (amax - amin)
+
+
+def walklevel(path, depth=1):
+    if depth < 0:
+        for root, dirs, files in os.walk(path):
+            yield root, dirs[:], files
+        return
+    elif depth == 0:
+        return
+    base_depth = path.rstrip(os.path.sep).count(os.path.sep)
+    for root, dirs, files in os.walk(path):
+        yield root, dirs[:], files
+        cur_depth = root.count(os.path.sep)
+        if base_depth + depth <= cur_depth:
+            del dirs[:]
+
+
+def subset_sessions(r, anm_pat, ss_pat):
+    anm_match = re.search(anm_pat, r["animal"])
+    ss_match = re.search(ss_pat, r["name"])
+    return bool(anm_match) and bool(ss_match)
+
+
+def xr_reset_index(ds, dim):
+    if type(dim) is not list:
+        dim = [dim]
+    for d in dim:
+        ds = ds.assign_coords({d: np.arange(ds.sizes[d])})
+    return ds
+
+
+def xr_nditer(arr, dims):
+    for key in itt.product(*[arr.coords[d].values for d in dims]):
+        asub = arr.sel({d: k for d, k in zip(dims, key)}).squeeze()
+        for d in asub.dims:
+            asub = asub.dropna(d, how="all")
+        if len(asub) > 0:
+            yield (key, asub)
+
+
+def enumerated_product(*args):
+    yield from zip(itt.product(*(range(len(x)) for x in args)), itt.product(*args))
