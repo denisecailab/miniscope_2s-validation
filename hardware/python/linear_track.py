a0c87fa66b150df5ab22330014047ce94961271e
phildong <zhe.dong@icahn.mssm.edu>
2022-10-13 11:10:35 -0400

    properly handle mask range

--- hardware/python/linear_track.py
+++ hardware/python/linear_track.py
@@ -428,8 +428,14 @@ def create_square_mask(self, h, w, outward_height, outward_width, center=None):
         center = (int(w / 2), int(h / 2))
         self._center = center
     mask = np.ones((h, w), dtype=bool)
-    width_range = (center[0] - outward_height, center[0] + outward_height)
-    length_range = (center[1] - outward_width, center[1] + outward_width)
+    width_range = (
+        max(0, center[0] - outward_height),
+        min(w, center[0] + outward_height),
+    )
+    length_range = (
+        max(0, center[1] - outward_width),
+        min(h, center[1] + outward_width),
+    )
     mask[length_range[0] : length_range[1], width_range[0] : width_range[1]] = False
     return mask
 
