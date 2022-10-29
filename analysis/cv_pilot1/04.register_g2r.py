1420134bd9b0768124fe2f2ad4ffdba6b24c095b
phildong <zhe.dong@icahn.mssm.edu>
2022-10-28 20:40:21 -0400

    migration from cv_pilot0 scripts

---
+++ analysis/cv_pilot1/04.register_g2r.py
@@ -0,0 +1,304 @@
+#%% imports and definitions
+import os
+import warnings
+
+import holoviews as hv
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import seaborn as sns
+import xarray as xr
+from minian.cross_registration import (
+    calculate_centroid_distance,
+    calculate_mapping,
+    group_by_session,
+)
+from minian.visualization import centroid
+from tqdm.auto import tqdm
+
+from routine.alignment import apply_affine, est_affine
+from routine.plotting import plot_overlap
+from routine.utilities import df_set_metadata
+
+IN_GREEN_PATH = "./intermediate/processed/green"
+IN_RED_PATH = "./intermediate/processed/red"
+IN_SS_FILE = "./log/sessions.csv"
+IN_RED_MAP = "./intermediate/cross_reg/red/mappings_meta.pkl"
+IN_GREEN_MAP = "./intermediate/cross_reg/green/mappings_meta.pkl"
+PARAM_DIST_THRES = 10
+OUT_PATH = "./intermediate/register_g2r"
+FIG_PATH = "./figs/register_g2r/"
+
+os.makedirs(OUT_PATH, exist_ok=True)
+os.makedirs(FIG_PATH, exist_ok=True)
+
+#%% load data and align
+red_path = os.path.join(OUT_PATH, "Ared")
+gn_path = os.path.join(OUT_PATH, "Agn")
+gn_trans_path = os.path.join(OUT_PATH, "Agn_trans")
+os.makedirs(red_path, exist_ok=True)
+os.makedirs(gn_path, exist_ok=True)
+os.makedirs(gn_trans_path, exist_ok=True)
+ss_df = pd.read_csv(IN_SS_FILE)
+ss_df = ss_df[ss_df["analyze"]]
+map_ls = []
+for anm, anm_df in tqdm(list(ss_df.groupby("animal"))):
+    plt_algn_dict = dict()
+    for _, row in tqdm(list(anm_df.iterrows()), leave=False):
+        ss = row["name"]
+        try:
+            green_ds = xr.open_dataset(
+                os.path.join(IN_GREEN_PATH, "{}-{}.nc".format(anm, ss))
+            ).squeeze()
+            red_ds = xr.open_dataset(
+                os.path.join(IN_RED_PATH, "{}-{}.nc".format(anm, ss))
+            ).squeeze()
+        except FileNotFoundError:
+            warnings.warn("Missing data for {} {}".format(anm, ss))
+            continue
+        # alignment
+        trans, _ = est_affine(green_ds["max_proj"], red_ds["max_proj"])
+        A_red = red_ds["A"].compute()
+        A_green = green_ds["A"].compute()
+        A_green_trans = xr.apply_ufunc(
+            apply_affine,
+            green_ds["A"],
+            input_core_dims=[["height", "width"]],
+            output_core_dims=[["height", "width"]],
+            vectorize=True,
+            kwargs={"tx": trans},
+        ).compute()
+        A_red.to_netcdf(os.path.join(red_path, "{}-{}.nc".format(anm, ss)))
+        A_green.to_netcdf(os.path.join(gn_path, "{}-{}.nc".format(anm, ss)))
+        A_green_trans.to_netcdf(os.path.join(gn_trans_path, "{}-{}.nc".format(anm, ss)))
+        # registration
+        cent_red = centroid(A_red)
+        cent_green = centroid(A_green_trans)
+        cent_red["session"] = "red"
+        cent_green["session"] = "green"
+        cents = pd.concat([cent_red, cent_green], ignore_index=True)
+        dist = calculate_centroid_distance(cents, index_dim=[])
+        dist = dist[dist["variable", "distance"] < PARAM_DIST_THRES]
+        dist = group_by_session(dist)
+        mapping = calculate_mapping(dist)
+        mapping.columns = mapping.columns.droplevel(0)
+        mapping = (
+            df_set_metadata(mapping, {"animal": anm, "session": ss})
+            .rename(columns={"green": "uid_green", "red": "uid_red"})
+            .drop(columns="group")
+        )
+        map_ls.append(mapping)
+mapping = pd.concat(map_ls, ignore_index=True)
+mapping.to_csv(os.path.join(OUT_PATH, "g2r_mapping.csv"), index=False)
+
+#%% compute green mapping based on red
+map_red = pd.read_pickle(IN_RED_MAP)
+map_green = pd.read_pickle(IN_GREEN_MAP)
+map_g2r = pd.read_csv(os.path.join(OUT_PATH, "g2r_mapping.csv")).set_index(
+    ["animal", "session", "uid_red"]
+)["uid_green"]
+map_green_reg = []
+for _, row in map_red.iterrows():
+    anm = row["meta", "animal"]
+    row_ss = row["session"]
+    row_new = row.copy()
+    idxs = [(anm, s, u) for s, u in zip(row_ss.index, row_ss.values)]
+    row_new.loc["session"] = map_g2r.reindex(idxs).fillna(-1).values
+    row_new.loc[row.isnull()] = np.nan
+    map_green_reg.append(row_new.to_frame().T)
+map_green_reg = pd.concat(map_green_reg, ignore_index=True)
+map_green_reg.to_pickle(os.path.join(OUT_PATH, "green_mapping_reg.pkl"))
+
+#%% plot cells
+im_opts = {"xaxis": None, "yaxis": None}
+red_path = os.path.join(OUT_PATH, "Ared")
+gn_path = os.path.join(OUT_PATH, "Agn")
+gn_trans_path = os.path.join(OUT_PATH, "Agn_trans")
+fig_algn_path = os.path.join(FIG_PATH, "alignment")
+fig_cells_path = os.path.join(FIG_PATH, "cells")
+os.makedirs(fig_algn_path, exist_ok=True)
+os.makedirs(fig_cells_path, exist_ok=True)
+map_red = pd.read_pickle(IN_RED_MAP)
+map_green = pd.read_pickle(IN_GREEN_MAP)
+map_g2r = pd.read_csv(os.path.join(OUT_PATH, "g2r_mapping.csv"))
+map_green_reg = pd.read_pickle(os.path.join(OUT_PATH, "green_mapping_reg.pkl"))
+cells_im = []
+for anm, anm_df in map_g2r.groupby("animal"):
+    plt_algn_dict = dict()
+    plt_cells_dict = dict()
+    for ss, mapping in tqdm(list(anm_df.groupby("session")), leave=False):
+        dsname = "{}-{}.nc".format(anm, ss)
+        A_red = xr.open_dataarray(os.path.join(red_path, dsname)).compute()
+        A_green = xr.open_dataarray(os.path.join(gn_path, dsname)).compute()
+        A_green_trans = xr.open_dataarray(os.path.join(gn_trans_path, dsname)).compute()
+        plt_algn_dict[(ss, "0.before_align")] = hv.RGB(
+            plot_overlap(A_green.max("unit_id"), A_red.max("unit_id"))
+        ).opts(**im_opts)
+        plt_algn_dict[(ss, "1.after_align")] = hv.RGB(
+            plot_overlap(A_green_trans.max("unit_id"), A_red.max("unit_id"))
+        ).opts(**im_opts)
+        plt_algn_dict[(ss, "2.registered")] = hv.RGB(
+            plot_overlap(
+                A_green_trans.sel(unit_id=mapping["uid_green"].values).max("unit_id"),
+                A_red.sel(unit_id=mapping["uid_red"].values).max("unit_id"),
+            )
+        ).opts(**im_opts)
+        idx_red = map_red[map_red["meta", "animal"] == anm]["session", ss].dropna()
+        idx_green = map_green_reg[map_green_reg["meta", "animal"] == anm][
+            "session", ss
+        ].dropna()
+        idx_green = idx_green[idx_green > 0]
+        c_ovly, c_gn, c_red = plot_overlap(
+            A_green_trans.sel(unit_id=np.array(idx_green)).max("unit_id"),
+            A_red.sel(unit_id=np.array(idx_red)).max("unit_id"),
+            return_raw=True,
+        )
+        plt_cells_dict[(ss, "0.red")] = hv.RGB(c_red).opts(**im_opts)
+        plt_cells_dict[(ss, "1.green")] = hv.RGB(c_gn).opts(**im_opts)
+        plt_cells_dict[(ss, "2.overlay")] = hv.RGB(c_ovly).opts(**im_opts)
+        cells_im.append(
+            pd.DataFrame(
+                {
+                    "animal": anm,
+                    "session": ss,
+                    "kind": ["red", "green", "ovly"],
+                    "im": [c_red, c_gn, c_ovly],
+                }
+            )
+        )
+    ovlp_plot = hv.NdLayout(plt_algn_dict, ["session", "reg"]).cols(6)
+    cells_plot = hv.NdLayout(plt_cells_dict, ["session", "kind"]).cols(6)
+    hv.save(ovlp_plot, os.path.join(fig_algn_path, "{}.html".format(anm)))
+    hv.save(cells_plot, os.path.join(fig_cells_path, "{}.html".format(anm)))
+cells_im = pd.concat(cells_im, ignore_index=True)
+cells_im.to_pickle(os.path.join(OUT_PATH, "cells_im.pkl"))
+#%% generate cells im figure
+def plot_cells(x, **kwargs):
+    ax = plt.gca()
+    ax.imshow(x.values[0])
+    ax.set_axis_off()
+
+
+fig_cells_path = os.path.join(FIG_PATH, "cells")
+os.makedirs(fig_cells_path, exist_ok=True)
+cells_im = pd.read_pickle(os.path.join(OUT_PATH, "cells_im.pkl"))
+for anm, anm_df in cells_im.groupby("animal"):
+    g = sns.FacetGrid(anm_df, row="kind", col="session", margin_titles=True)
+    g.map(plot_cells, "im")
+    fig = g.fig
+    fig.tight_layout()
+    fig.savefig(
+        os.path.join(fig_cells_path, "{}.svg".format(anm)), dpi=500, bbox_inches="tight"
+    )
+
+#%% plot results
+def find_active(df):
+    df["variable", "nactive"] = (df["session"] >= 0).sum(axis="columns")
+    df["variable", "npresent"] = df["session"].notnull().sum(axis="columns")
+    nsess = df["variable", "npresent"].max()
+    df["variable", "nsess"] = nsess
+    return df
+
+
+map_red = pd.read_pickle(IN_RED_MAP)
+map_green = pd.read_pickle(IN_GREEN_MAP)
+map_green_reg = pd.read_pickle(os.path.join(OUT_PATH, "green_mapping_reg.pkl"))
+map_red["meta", "method"] = "red/raw"
+map_green["meta", "method"] = "green/raw"
+map_green_reg["meta", "method"] = "red/registered"
+map_red = map_red.groupby(("meta", "animal")).apply(find_active)
+map_green = map_green.groupby(("meta", "animal")).apply(find_active)
+map_green_reg = map_green_reg.groupby(("meta", "animal")).apply(find_active)
+map_red["variable", "stable"] = True
+map_green["variable", "stable"] = True
+map_green_reg["variable", "stable"] = (
+    map_green_reg["variable", "npresent"] == map_green_reg["variable", "nsess"]
+)
+map_red["variable", "pactive"] = (
+    map_red["variable", "nactive"] / map_red["variable", "nsess"]
+)
+map_green["variable", "pactive"] = (
+    map_green["variable", "nactive"] / map_green["variable", "nsess"]
+)
+map_green_reg["variable", "pactive"] = (
+    map_green_reg["variable", "nactive"] / map_green_reg["variable", "npresent"]
+)
+map_master = pd.concat([map_green_reg, map_green, map_red], ignore_index=True)
+map_master.columns = map_master.columns.droplevel(0)
+fig_nactive = px.histogram(
+    map_master[map_master["stable"]],
+    x="nactive",
+    facet_col="animal",
+    facet_row="method",
+    range_x=[-0.5, 10.5],
+    range_y=[0, 0.6],
+    histnorm="probability",
+    labels={"nactive": "Number of<br>Active Sessions"},
+)
+fig_pactive = px.histogram(
+    map_master,
+    x="pactive",
+    facet_col="animal",
+    facet_row="method",
+    range_x=[-0.1, 1.1],
+    range_y=[0, 0.6],
+    histnorm="probability",
+    labels={"pactive": "Probability of<br>Activation"},
+)
+fig_nactive.write_html(os.path.join(FIG_PATH, "nactive.html"))
+fig_pactive.write_html(os.path.join(FIG_PATH, "pactive.html"))
+
+#%% plot summary
+def find_active(df):
+    df["variable", "nactive"] = (df["session"] >= 0).sum(axis="columns")
+    df["variable", "npresent"] = df["session"].notnull().sum(axis="columns")
+    nsess = df["variable", "npresent"].max()
+    df["variable", "nsess"] = nsess
+    return df
+
+
+def agg_pactive(df):
+    df_agg = df.groupby("pactive").size().reset_index(name="counts")
+    df_agg["density"] = df_agg["counts"] / df_agg["counts"].sum()
+    return df_agg.set_index("pactive")
+
+
+map_red = pd.read_pickle(IN_RED_MAP)
+map_green = pd.read_pickle(IN_GREEN_MAP)
+map_green_reg = pd.read_pickle(os.path.join(OUT_PATH, "green_mapping_reg.pkl"))
+map_red = map_red.groupby(("meta", "animal")).apply(find_active)
+map_green = map_green.groupby(("meta", "animal")).apply(find_active)
+map_green_reg = map_green_reg.groupby(("meta", "animal")).apply(find_active)
+map_red["variable", "pactive"] = (
+    map_red["variable", "nactive"] / map_red["variable", "nsess"]
+)
+map_green["variable", "pactive"] = (
+    map_green["variable", "nactive"] / map_green["variable", "nsess"]
+)
+map_green_reg["variable", "pactive"] = (
+    map_green_reg["variable", "nactive"] / map_green_reg["variable", "npresent"]
+)
+map_red.columns = map_red.columns.droplevel(0)
+map_green.columns = map_green.columns.droplevel(0)
+map_green_reg.columns = map_green_reg.columns.droplevel(0)
+red_agg = map_red.groupby("animal").apply(agg_pactive).reset_index()
+green_agg = map_green.groupby("animal").apply(agg_pactive).reset_index()
+green_reg_agg = map_green_reg.groupby("animal").apply(agg_pactive).reset_index()
+red_agg["method"] = "tdTomato channel"
+green_agg["method"] = "GCaMP channel"
+green_reg_agg["method"] = "GCaMP cells\nregistered to tdTomato"
+agg_df = pd.concat([red_agg, green_agg, green_reg_agg], ignore_index=True)
+g = sns.FacetGrid(agg_df, col="method")
+g.map_dataframe(
+    sns.histplot,
+    x="pactive",
+    weights="density",
+    stat="probability",
+    bins=8,
+    binrange=(0, 1),
+    kde=True,
+)
+g.set_titles("{col_name}")
+g.set_axis_labels(x_var="Proportion of\nsessions active", y_var="Proportion of cells")
+g.fig.savefig(os.path.join(FIG_PATH, "summary.svg"), dpi=500, bbox_inches="tight")
---
+++ analysis/cv_pilot1/routine/plotting.py
@@ -0,0 +1,379 @@
+import colorcet as cc
+import cv2
+import holoviews as hv
+import numpy as np
+import plotly.express as px
+import xarray as xr
+from matplotlib import cm
+from plotly import graph_objects as go
+
+hv.notebook_extension("bokeh")
+
+
+def plotA_contour(A: xr.DataArray, im: xr.DataArray, cmap=None, im_opts=None):
+    im = hv.Image(im, ["width", "height"])
+    if im_opts is not None:
+        im = im.opts(**im_opts)
+    im = im * hv.Path([])
+    for uid in A.coords["unit_id"].values:
+        curA = (np.array(A.sel(unit_id=uid)) > 0).astype(np.uint8)
+        cnt = cv2.findContours(curA, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[0][
+            0
+        ].squeeze()
+        if cnt.ndim > 1:
+            pth = hv.Path(cnt.squeeze())
+            if cmap is not None:
+                pth = pth.opts(color=cmap[uid])
+            im = im * pth
+    return im
+
+
+def plot_overlap(
+    im_green: np.ndarray, im_red: np.ndarray, brt_offset=0, return_raw=False
+):
+    im_red = np.clip(
+        cm.ScalarMappable(cmap=cc.m_linear_ternary_red_0_50_c52).to_rgba(
+            np.array(im_red)
+        )
+        + brt_offset,
+        0,
+        1,
+    )
+    im_green = np.clip(
+        cm.ScalarMappable(cmap=cc.m_linear_ternary_green_0_46_c42).to_rgba(
+            np.array(im_green)
+        )
+        + brt_offset,
+        0,
+        1,
+    )
+    ovly = np.clip(im_red + im_green, 0, 1)
+    if return_raw:
+        return ovly, im_green, im_red
+    else:
+        return ovly
+
+
+import copy
+
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+from .utilities import enumerated_product
+
+
+def facet_plotly(
+    data: pd.DataFrame,
+    facet_row: str,
+    facet_col: str,
+    title_dim: str = None,
+    specs: dict = None,
+    col_wrap: int = None,
+    **kwargs,
+):
+    row_crd = data[facet_row].unique()
+    col_crd = data[facet_col].unique()
+    layout_ls = []
+    iiter = 0
+    for (ir, ic), (r, c) in enumerated_product(row_crd, col_crd):
+        dat_sub = data[(data[facet_row] == r) & (data[facet_col] == c)]
+        if not len(dat_sub) > 0:
+            continue
+        if title_dim is not None:
+            title = dat_sub[title_dim].unique().item()
+        else:
+            if facet_row == "DUMMY_FACET_ROW":
+                title = "{}={}".format(facet_col, c)
+            elif facet_col == "DUMMY_FACET_COL":
+                title = "{}={}".format(facet_row, r)
+            else:
+                title = "{}={}; {}={}".format(facet_row, r, facet_col, c)
+        if col_wrap is not None:
+            ir = iiter // col_wrap
+            ic = iiter % col_wrap
+            iiter += 1
+        layout_ls.append(
+            {"row": ir, "col": ic, "row_label": r, "col_label": c, "title": title}
+        )
+    layout = pd.DataFrame(layout_ls).set_index(["row_label", "col_label"])
+    if col_wrap is not None:
+        nrow, ncol = int(layout["row"].max() + 1), int(layout["col"].max() + 1)
+    else:
+        nrow, ncol = len(row_crd), len(col_crd)
+    if specs is not None:
+        specs = np.full((nrow, ncol), specs).tolist()
+    fig = make_subplots(
+        rows=nrow,
+        cols=ncol,
+        subplot_titles=layout["title"].values,
+        specs=specs,
+        **kwargs,
+    )
+    return fig, layout
+
+
+def handle_single_facet(data, facet_row, facet_col):
+    data = data.copy()
+    if facet_row is None:
+        data["DUMMY_FACET_ROW"] = ""
+        facet_row = "DUMMY_FACET_ROW"
+    if facet_col is None:
+        data["DUMMY_FACET_COL"] = ""
+        facet_col = "DUMMY_FACET_COL"
+    return data, facet_row, facet_col
+
+
+def scatter_3d(
+    data, facet_row, facet_col, legend_dim: str = None, title_dim: str = None, **kwargs
+):
+    data, facet_row, facet_col = handle_single_facet(data, facet_row, facet_col)
+    fig, layout = facet_plotly(
+        data, facet_row, facet_col, title_dim, specs={"type": "scene"}
+    )
+    if legend_dim is not None:
+        show_legend = {l: True for l in data[legend_dim].unique()}
+    for (rlab, clab), facet_df in data.groupby([facet_row, facet_col], observed=True):
+        ly = layout.loc[(rlab, clab), :]
+        ir, ic = ly["row"], ly["col"]
+        if legend_dim is not None:
+            for llab, subdf in facet_df.groupby(legend_dim, observed=True):
+                show_leg = show_legend[llab]
+                cur_args = transform_arguments(subdf, kwargs)
+                trace = go.Scatter3d(
+                    name=llab, legendgroup=llab, showlegend=show_leg, **cur_args
+                )
+                if show_leg:
+                    show_legend[llab] = False
+                fig.add_trace(trace, row=ir + 1, col=ic + 1)
+        else:
+            cur_args = transform_arguments(facet_df, kwargs)
+            trace = go.Scatter3d(showlegend=False, **cur_args)
+            fig.add_trace(trace, row=ir + 1, col=ic + 1)
+    return fig
+
+
+def transform_arguments(data: pd.DataFrame, arguments: dict):
+    arg_ret = copy.deepcopy(arguments)
+    if arg_ret.get("x"):
+        arg_ret["x"] = data[arg_ret["x"]].values
+    if arg_ret.get("y"):
+        arg_ret["y"] = data[arg_ret["y"]].values
+    if arg_ret.get("z"):
+        arg_ret["z"] = data[arg_ret["z"]].values
+    if arg_ret.get("error_x"):
+        x_err = data[arg_ret["error_x"]].values
+        if np.nansum(x_err) > 0:
+            arg_ret["error_x"] = {"array": x_err}
+        else:
+            del arg_ret["error_x"]
+    if arg_ret.get("error_y"):
+        y_err = data[arg_ret["error_y"]].values
+        if np.nansum(y_err) > 0:
+            arg_ret["error_y"] = {"array": y_err}
+        else:
+            del arg_ret["error_y"]
+    if arg_ret.get("text"):
+        try:
+            arg_ret["text"] = data[arg_ret["text"]].values
+        except KeyError:
+            del arg_ret["text"]
+    mkopts = arg_ret.get("marker")
+    if mkopts:
+        if mkopts.get("color"):
+            try:
+                color = data[mkopts["color"]].values
+                if len(np.unique(color)) == 1:
+                    mkopts["color"] = np.unique(color).item()
+                else:
+                    mkopts["color"] = color
+            except KeyError:
+                pass
+        if mkopts.get("size"):
+            try:
+                size = data[mkopts["size"]].values
+                if len(np.unique(size)) == 1:
+                    mkopts["size"] = np.unique(size).item()
+                else:
+                    mkopts["size"] = size
+            except KeyError:
+                pass
+        if mkopts.get("symbol"):
+            try:
+                symb = data[mkopts["symbol"]].values
+                if len(np.unique(symb)) == 1:
+                    mkopts["symbol"] = np.unique(symb).item()
+                else:
+                    mkopts["symbol"] = symb
+            except KeyError:
+                pass
+    return arg_ret
+
+
+def scatter_agg(
+    data,
+    x,
+    y,
+    facet_row,
+    facet_col,
+    col_wrap: int = None,
+    legend_dim: str = None,
+    title_dim: str = None,
+    show_point_legend=False,
+    subplot_args: dict = dict(),
+    **kwargs,
+):
+    data, facet_row, facet_col = handle_single_facet(data, facet_row, facet_col)
+    fig, layout = facet_plotly(
+        data, facet_row, facet_col, title_dim, col_wrap=col_wrap, **subplot_args
+    )
+    grp_dims = [facet_row, facet_col, x]
+    idx_dims = [facet_row, facet_col]
+    if legend_dim is not None:
+        grp_dims.append(legend_dim)
+        idx_dims.append(legend_dim)
+        show_legend = {l: True for l in data[legend_dim].unique()}
+    data_agg = (
+        data.groupby(grp_dims)[y]
+        .agg(["mean", "sem"])
+        .reset_index()
+        .merge(data, on=grp_dims)
+        .set_index(idx_dims)
+        .sort_index()
+    )
+    kwargs["x"] = x
+    kwargs["y"] = y
+    kwargs_agg = copy.deepcopy(kwargs)
+    kwargs_agg["y"] = "mean"
+    kwargs_agg["error_y"] = "sem"
+    for (rlab, clab), facet_df in data.groupby([facet_row, facet_col], observed=True):
+        ly = layout.loc[(rlab, clab), :]
+        ir, ic = ly["row"], ly["col"]
+        trace_ls = []
+        if legend_dim is not None:
+            for llab, subdf in facet_df.groupby(legend_dim, observed=True):
+                show_leg = show_legend[llab]
+                cur_args = transform_arguments(subdf, kwargs)
+                ndata = subdf.groupby(x)[y].count().max()
+                if ndata > 1:
+                    strp = go.Box(
+                        name=llab,
+                        legendgroup=llab,
+                        showlegend=show_leg and show_point_legend,
+                        boxpoints="all",
+                        fillcolor="rgba(255,255,255,0)",
+                        hoveron="points",
+                        line={"color": "rgba(255,255,255,0)"},
+                        pointpos=0,
+                        opacity=0.4,
+                        **cur_args,
+                    )
+                    trace_ls.append(strp)
+                subdf_agg = data_agg.loc[rlab, clab, llab].reset_index()
+                cur_args = transform_arguments(subdf_agg, kwargs_agg)
+                ln = go.Scatter(
+                    name=llab,
+                    legendgroup=llab,
+                    showlegend=show_leg,
+                    mode="lines",
+                    **cur_args,
+                )
+                trace_ls.append(ln)
+                if show_leg:
+                    show_legend[llab] = False
+        else:
+            cur_args = transform_arguments(facet_df, kwargs)
+            ndata = facet_df.groupby(x)[y].count().max()
+            if ndata > 1:
+                strp = go.Box(
+                    boxpoints="all",
+                    fillcolor="rgba(255,255,255,0)",
+                    hoveron="points",
+                    line={"color": "rgba(255,255,255,0)"},
+                    pointpos=0,
+                    opacity=0.4,
+                    **cur_args,
+                )
+                trace_ls.append(strp)
+            facet_df_agg = data_agg.loc[rlab, clab].reset_index()
+            cur_args = transform_arguments(facet_df_agg, kwargs_agg)
+            ln = go.Scatter(mode="lines", **cur_args)
+            trace_ls.append(ln)
+        for t in trace_ls:
+            fig.add_trace(t, row=ir + 1, col=ic + 1)
+    return fig
+
+
+def imshow(
+    data,
+    facet_row,
+    facet_col,
+    title_dim: str = None,
+    subplot_args: dict = dict(),
+    **kwargs,
+):
+    data, facet_row, facet_col = handle_single_facet(data, facet_row, facet_col)
+    fig, layout = facet_plotly(data, facet_row, facet_col, title_dim, **subplot_args)
+    for (rlab, clab), facet_df in data.groupby([facet_row, facet_col], observed=True):
+        ly = layout.loc[(rlab, clab), :]
+        ir, ic = ly["row"], ly["col"]
+        cur_args = transform_arguments(facet_df, kwargs)
+        trace = go.Heatmap(**cur_args)
+        fig.add_trace(trace, row=ir + 1, col=ic + 1)
+    return fig
+
+
+def line(error_y_mode=None, **kwargs):
+    """Extension of `plotly.express.line` to use error bands."""
+    ERROR_MODES = {"bar", "band", "bars", "bands", None}
+    if error_y_mode not in ERROR_MODES:
+        raise ValueError(
+            f"'error_y_mode' must be one of {ERROR_MODES}, received {repr(error_y_mode)}."
+        )
+    if error_y_mode in {"bar", "bars", None}:
+        fig = px.line(**kwargs)
+    elif error_y_mode in {"band", "bands"}:
+        if "error_y" not in kwargs:
+            raise ValueError(
+                f"If you provide argument 'error_y_mode' you must also provide 'error_y'."
+            )
+        figure_with_error_bars = px.line(**kwargs)
+        fig = px.line(**{arg: val for arg, val in kwargs.items() if arg != "error_y"})
+        for data in figure_with_error_bars.data:
+            x = list(data["x"])
+            y_upper = list(data["y"] + data["error_y"]["array"])
+            y_lower = list(
+                data["y"] - data["error_y"]["array"]
+                if data["error_y"]["arrayminus"] is None
+                else data["y"] - data["error_y"]["arrayminus"]
+            )
+            color = (
+                f"rgba({tuple(int(data['line']['color'].lstrip('#')[i:i+2], 16) for i in (0, 2, 4))},.3)".replace(
+                    "((", "("
+                )
+                .replace("),", ",")
+                .replace(" ", "")
+            )
+            fig.add_trace(
+                go.Scatter(
+                    x=x + x[::-1],
+                    y=y_upper + y_lower[::-1],
+                    fill="toself",
+                    fillcolor=color,
+                    line=dict(color="rgba(255,255,255,0)"),
+                    hoverinfo="skip",
+                    showlegend=False,
+                    legendgroup=data["legendgroup"],
+                    xaxis=data["xaxis"],
+                    yaxis=data["yaxis"],
+                )
+            )
+        # Reorder data as said here: https://stackoverflow.com/a/66854398/8849755
+        reordered_data = []
+        for i in range(int(len(fig.data) / 2)):
+            reordered_data.append(fig.data[i + int(len(fig.data) / 2)])
+            reordered_data.append(fig.data[i])
+        fig.data = tuple(reordered_data)
+    return fig
