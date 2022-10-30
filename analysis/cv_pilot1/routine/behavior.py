3ad23060178e48109919e346fc56678cf58405c3
phildong <zhe.dong@icahn.mssm.edu>
2022-10-29 22:31:56 -0400

    update behavior processing routine

--- analysis/cv_pilot1/routine/behavior.py
+++ analysis/cv_pilot1/routine/behavior.py
@@ -1,52 +1,9 @@
+import re
+
 import numpy as np
 import pandas as pd
-from scipy.signal import medfilt
 from scipy.ndimage import label
-
-
-def merge_ts(ms_ts: pd.DataFrame, behav_ts: pd.DataFrame) -> pd.DataFrame:
-    ms_ts["camNum"] = 0
-    behav_ts["camNum"] = 1
-    return pd.concat([ms_ts, behav_ts]).rename(
-        columns={"Frame Number": "frameNum", "Time Stamp (ms)": "sysClock"}
-    )
-
-
-def map_ts(ts: pd.DataFrame) -> pd.DataFrame:
-    """
-    map frames from Cam1 to Cam0 with nearest neighbour using the timestamp
-    file from miniscope recordings.
-    Parameters
-    ----------
-    ts : pd.DataFrame
-        input timestamp dataframe. should contain field 'frameNum', 'camNum' and
-        'sysClock'
-    Returns
-    -------
-    pd.DataFrame
-        output dataframe. should contain field 'fmCam0' and 'fmCam1'
-    """
-    ts_sort = ts.sort_values("sysClock")
-    ts_sort["ts_behav"] = np.where(ts_sort["camNum"] == 1, ts_sort["sysClock"], np.nan)
-    ts_sort["ts_forward"] = ts_sort["ts_behav"].fillna(method="ffill")
-    ts_sort["ts_backward"] = ts_sort["ts_behav"].fillna(method="bfill")
-    ts_sort["diff_forward"] = np.absolute(ts_sort["sysClock"] - ts_sort["ts_forward"])
-    ts_sort["diff_backward"] = np.absolute(ts_sort["sysClock"] - ts_sort["ts_backward"])
-    ts_sort["fm_behav"] = np.where(ts_sort["camNum"] == 1, ts_sort["frameNum"], np.nan)
-    ts_sort["fm_forward"] = ts_sort["fm_behav"].fillna(method="ffill")
-    ts_sort["fm_backward"] = ts_sort["fm_behav"].fillna(method="bfill")
-    ts_sort["fmCam1"] = np.where(
-        ts_sort["diff_forward"] < ts_sort["diff_backward"],
-        ts_sort["fm_forward"],
-        ts_sort["fm_backward"],
-    )
-    ts_map = (
-        ts_sort[ts_sort["camNum"] == 0][["frameNum", "fmCam1"]]
-        .dropna()
-        .rename(columns=dict(frameNum="fmCam0"))
-        .astype(dict(fmCam1=int))
-    )
-    return ts_map
+from scipy.signal import medfilt
 
 
 def linearize_pos(xy):
@@ -59,24 +16,64 @@ def linearize_pos(xy):
 
 def code_direction(pos, smooth, diff_thres=None):
     pos_smooth = medfilt(pos, smooth)
-    diff = np.diff(pos_smooth)
-    diff = np.concatenate([[diff[0]], diff])
+    diff = medfilt(np.gradient(pos_smooth, 5), smooth)
     pos_sign = pos_smooth * np.sign(diff)
     if diff_thres is not None:
-        pos_sign = np.where(np.abs(diff) > diff_thres, pos_sign, np.nan)
+        running_mask = np.zeros_like(pos_sign, dtype=bool)
+        diff_mag = np.abs(diff)
+        for rng, thres in diff_thres.items():
+            running_mask[
+                np.logical_and.reduce(
+                    [pos_smooth >= rng[0], pos_smooth <= rng[1], diff_mag > thres]
+                )
+            ] = True
+        pos_sign = np.where(running_mask, pos_sign, np.nan)
     return pos_sign
 
 
-def determine_trial(pos, smooth, rw_low=10, rw_high=90, min_time=2 * 60):
-    pos = medfilt(pos, smooth)
-    rw = np.full_like(pos, np.nan)
-    rw = np.where(pos < rw_low, 0, rw)
-    rw = np.where(pos > rw_high, 1, rw)
-    start = rw[~np.isnan(rw)][0]
-    trial, ntrial = label(rw == start)
-    for t in range(ntrial):
-        tidx = np.where(trial == t + 1)[0]
-        trial[tidx] = 0
-        if len(tidx) > min_time:
-            trial[tidx[0]] = 1
-    return trial.cumsum()
+def determine_trial(df):
+    rw_first = df[df["event"] == "REWARD"].iloc[0]
+    df["trial"] = 0
+    df.loc[(df["event"] == "REWARD") & (df["data"] == rw_first["data"]), "trial"] = 1
+    df["trial"] = df["trial"].cumsum()
+    return df
+
+
+def extract_location(r):
+    if type(r["data"]) is not str:
+        return pd.Series([], dtype=float)
+    match = re.search(r"X(?P<x>\d+)Y(?P<y>\d+)", r["data"])
+    if match:
+        return pd.Series(match.groupdict())
+    else:
+        return pd.Series([], dtype=float)
+
+
+def label_ts(behav, ms_ts):
+    behav = behav.copy()
+    ms_ts = ms_ts.copy()
+    tstart = behav[behav["event"] == "START"]["timestamp"].item()
+    tterminate = behav[behav["event"] == "TERMINATE"]["timestamp"].item()
+    behav = behav[behav["timestamp"].between(tstart, tterminate, inclusive="both")]
+    behav["timestamp"] = (behav["timestamp"] - tterminate) * 1e3
+    ms_ts["timestamp"] = ms_ts["timestamp"] - ms_ts["timestamp"].iloc[-1].item()
+    fm_cut = (ms_ts["timestamp"].values[1:] + ms_ts["timestamp"].values[:-1]) / 2
+    fm_cut = np.append(np.insert(fm_cut, 0, ms_ts["timestamp"].iloc[0].item()), 0)
+    behav["ms_frame"] = pd.cut(behav["timestamp"], fm_cut, labels=ms_ts["frame"])
+    return behav[behav["ms_frame"].notnull()]
+
+
+def agg_behav(df):
+    if len(df) > 0:
+        return pd.Series(
+            {
+                "x": df["x"].median(),
+                "y": df["y"].median(),
+                "trial": df["trial"].mode().values[0],
+            }
+        )
+    else:
+        return pd.Series(
+            data=np.full(3, np.nan),
+            index=["x", "y", "trial"],
+        )
