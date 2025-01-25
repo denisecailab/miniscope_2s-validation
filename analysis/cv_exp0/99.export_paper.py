4c8386ce1df30482221540e96cdb18a0cc2d0815
phildong <35715936+phildong@users.noreply.github.com>
2025-01-25 12:20:49 -0500

    add rrow paper figure

--- analysis/cv_exp0/99.export_paper.py
+++ analysis/cv_exp0/99.export_paper.py
@@ -122,3 +122,29 @@ fig = Figure(
     panB.move(x=(w_fig - panB.width) / 2, y=panA.height + h_gap),
 )
 fig.save(os.path.join(OUT_PATH, "figS1.svg"))
+
+# %% make rrow figure
+w_gap = 10
+h_gap = 25
+sh_left = (0, 0)
+svgs = {
+    "A": "./figs/external/rrow_task.svg",
+    "B": "./figs/behav_comparison/rrow.svg",
+}
+for fn in svgs.values():
+    svg_unique_id(fn)
+
+panA = make_svg_panel("A", svgs["A"], PARAMT_TEXT, im_scale=1.2)
+panB = make_svg_panel("B", svgs["B"], PARAMT_TEXT, sh=sh_left)
+panA = make_svg_panel(
+    "A", svgs["A"], PARAMT_TEXT, im_scale=1.2, sh=((panB.width - panA.width) / 2, 0)
+)
+h_fig = panA.height + panB.height + h_gap
+w_fig = max(panA.width, panB.width)
+fig = Figure(
+    w_fig,
+    h_fig,
+    panA,
+    panB.move(x=0, y=panA.height + h_gap),
+)
+fig.save(os.path.join(OUT_PATH, "rrow.svg"))
